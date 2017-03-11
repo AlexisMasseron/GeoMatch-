0a309164c8ce2b7be0e2f644b6dbc5aa351aac4c
Alexis Masseron <alexismasseron@gmail.com>
2017-03-11 18:06:06 +0100

    implemented pushes and promises

--- GeoMatch.sol
+++ GeoMatch.sol
@@ -7,29 +7,49 @@ contract GeoMatch{
       bytes line;
       bytes point;
     }
-    
-    Geohash[] globalHash;
-    
+
+  Geohash[] globalHash;
+
   function GeoMatch(){
 
-    globalHash.polygone = addPolygone();
-    globalHash.line = addLine();
-    globalHash.point = addPoint();
+  }
 
+  function sendHash(){
+    addPolygone();
+    addLine();
+    addPoint();
     return globalHash;
-
   }
 
   function addPolygone(){
-    return ["QmNr5ZsrfnK945coeMPZNXj9qHm8YyvdQRqUbgLTsHUnb1", "QmXVaEhjtfsCZ3pPNPf2LKR3U9rqEyNXY2TdxW4Ad9p37u"];
+    globalHash.polygone.push("QmNr5ZsrfnK945coeMPZNXj9qHm8YyvdQRqUbgLTsHUnb1", "QmXVaEhjtfsCZ3pPNPf2LKR3U9rqEyNXY2TdxW4Ad9p37u");
+    return new Promise  (
+      function(resolve, reject){
+        resolve(globalHash.polygone.push() );
+        reject(console.log('error pushing the polygone in the array'));
+      }
+    )
   }
 
   function addLine(){
-    return ["QmWHGnYtCkjgeevw9udPgoYb51KUA32TcNuhSVymSFgHej", "QmPof8rDuRPCYc2BgEhP1dDNXPtPCzFKfGHunTc74npypx"];
+    globalHash.line.push("QmWHGnYtCkjgeevw9udPgoYb51KUA32TcNuhSVymSFgHej", "QmPof8rDuRPCYc2BgEhP1dDNXPtPCzFKfGHunTc74npypx");
+    return new Promise  (
+      function(resolve, reject){
+        resolve(globalHash.line.push() );
+        reject(console.log('error pushing the line in the array'));
+      }
+    )
   }
 
   function addPoint(){
-    return ["QmZpoCrgWntmxBQL5RAEWy4Y9LXMrna4eS8dyBvxhkvowz", "QmSmy1jcEgewCiJVKFHabANScUF7Mn3GcWxbzgfbR9jVJR"];
+
+    globalHash.line.push("QmZpoCrgWntmxBQL5RAEWy4Y9LXMrna4eS8dyBvxhkvowz", "QmSmy1jcEgewCiJVKFHabANScUF7Mn3GcWxbzgfbR9jVJR");
+    return new Promise  (
+      function(resolve, reject){
+        resolve(globalHash.line.push() );
+        reject(console.log('error pushing the point the array'));
+      }
+    )
   }
 
 
