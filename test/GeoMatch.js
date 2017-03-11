5c93f9c74535d6e21819e6516e83e547571efa4d
Fabien Champigny <fabien@champigny.name>
2017-03-11 18:55:30 +0100

    fix

--- test/GeoMatch.js
+++ test/GeoMatch.js
@@ -1,80 +1,96 @@
 var MetaCoin = artifacts.require("./GeoMatch.sol");
 
-contract('GeoMatch', function(accounts) {
+contract('GeoMatch', function (accounts) {
 
-  it.only("Deploy test", function() {
+    it.skip("Deploy test", function () {
 
-    return MetaCoin.deployed()
-        .then(function(instance){
-            return instance.isAlive();
-        })
-        .then(function(alive) {
-            assert.equal(alive, true);
-        })
-  });
-
-  /*
-  it.skip('demo', function() {
-      return MetaCoin.deployed().then(function(instance) {
-          return instance.getBalance.call(accounts[0]);
-      }).then(function(balance) {
-          assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
-      });
-  })
-
-
-
-  it.skip("should call a function that depends on a linked library", function() {
-    var meta;
-    var metaCoinBalance;
-    var metaCoinEthBalance;
-
-    return MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(accounts[0]);
-    }).then(function(outCoinBalance) {
-      metaCoinBalance = outCoinBalance.toNumber();
-      return meta.getBalanceInEth.call(accounts[0]);
-    }).then(function(outCoinBalanceEth) {
-      metaCoinEthBalance = outCoinBalanceEth.toNumber();
-    }).then(function() {
-      assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, "Library function returned unexpected function, linkage may be broken");
+        return MetaCoin.deployed()
+            .then(function (instance) {
+                return instance.isAlive();
+            })
+            .then(function (alive) {
+                assert.isTrue(alive);
+            })
     });
-  });
-  it.skip("should send coin correctly", function() {
-    var meta;
 
-    // Get initial balances of first and second account.
-    var account_one = accounts[0];
-    var account_two = accounts[1];
+    it('addGeoHash', function () {
 
-    var account_one_starting_balance;
-    var account_two_starting_balance;
-    var account_one_ending_balance;
-    var account_two_ending_balance;
+        var polygone = 'QmTjXvDjPtaSP2G1yiVwcugEMBaoVT4q9ZPxuQQBHsYgUw';
+        var line = 'QmNr5ZsrfnK945coeMPZNXj9qHm8YyvdQRqUbgLTsHUnb1';
+        var point = 'QmXVaEhjtfsCZ3pPNPf2LKR3U9rqEyNXY2TdxW4Ad9p37u';
 
-    var amount = 10;
+        return MetaCoin.deployed()
+            .then(function (instance) {
+                return instance.addGeoHash(polygone, line, point);
+            })
+            .then(function (result) {
+                console.log(result);
+                assert.isTrue(true);
+            })
+    })
 
-    return MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_starting_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_starting_balance = balance.toNumber();
-      return meta.sendCoin(account_two, amount, {from: account_one});
-    }).then(function() {
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_ending_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_ending_balance = balance.toNumber();
+    /*
+     it.skip('demo', function() {
+     return MetaCoin.deployed().then(function(instance) {
+     return instance.getBalance.call(accounts[0]);
+     }).then(function(balance) {
+     assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+     });
+     })
 
-      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
-    });
-  });
-  */
+
+
+     it.skip("should call a function that depends on a linked library", function() {
+     var meta;
+     var metaCoinBalance;
+     var metaCoinEthBalance;
+
+     return MetaCoin.deployed().then(function(instance) {
+     meta = instance;
+     return meta.getBalance.call(accounts[0]);
+     }).then(function(outCoinBalance) {
+     metaCoinBalance = outCoinBalance.toNumber();
+     return meta.getBalanceInEth.call(accounts[0]);
+     }).then(function(outCoinBalanceEth) {
+     metaCoinEthBalance = outCoinBalanceEth.toNumber();
+     }).then(function() {
+     assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, "Library function returned unexpected function, linkage may be broken");
+     });
+     });
+     it.skip("should send coin correctly", function() {
+     var meta;
+
+     // Get initial balances of first and second account.
+     var account_one = accounts[0];
+     var account_two = accounts[1];
+
+     var account_one_starting_balance;
+     var account_two_starting_balance;
+     var account_one_ending_balance;
+     var account_two_ending_balance;
+
+     var amount = 10;
+
+     return MetaCoin.deployed().then(function(instance) {
+     meta = instance;
+     return meta.getBalance.call(account_one);
+     }).then(function(balance) {
+     account_one_starting_balance = balance.toNumber();
+     return meta.getBalance.call(account_two);
+     }).then(function(balance) {
+     account_two_starting_balance = balance.toNumber();
+     return meta.sendCoin(account_two, amount, {from: account_one});
+     }).then(function() {
+     return meta.getBalance.call(account_one);
+     }).then(function(balance) {
+     account_one_ending_balance = balance.toNumber();
+     return meta.getBalance.call(account_two);
+     }).then(function(balance) {
+     account_two_ending_balance = balance.toNumber();
+
+     assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
+     assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+     });
+     });
+     */
 });
