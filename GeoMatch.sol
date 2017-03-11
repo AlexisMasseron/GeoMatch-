30d4d5381f71bc4b8e2c1a001ab6cc4509e63778
Alexis Masseron <alexismasseron@gmail.com>
2017-03-11 15:33:50 +0100

    added contract template

---
+++ GeoMatch.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.4;
+
+contract GeoMatch{
+
+  function GeoMatch(){
+    struct {
+      uint[] polygone;
+      uint[] line;
+      uint[] point;
+    }
+  }
+
+  function addPolygone(){
+
+  }
+
+  function addLine(){
+
+  }
+
+  function addPoint(){
+
+  }
+
+
+}
